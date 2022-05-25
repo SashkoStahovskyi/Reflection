47433566cda8858ab29a65dc368ee044d77ba049
airavata-elephant <abhramatanga@gmail.com>
2022-05-25 21:02:23 +0300

    First commit

---
+++ src/main/java/reflection/Person.java
@@ -0,0 +1,37 @@
+package reflection;
+
+public class Person {
+    private String name = "Sasha";
+    private int age = 25;
+    private boolean gay = true;
+
+
+    public void sayHello() {
+        System.out.println("Aloha!");
+    }
+
+    public void sayBay() {
+        System.out.println("See you!");
+    }
+
+    final void yo() {
+        System.out.println("yo Man!");
+    }
+
+    final void sayRap() {
+        System.out.println("Tag Life!");
+    }
+
+    final void sa() {
+        System.out.println("Tag Life!");
+    }
+
+    static void kj() {
+    }
+
+    private void uk() {
+    }
+
+}
+
+
